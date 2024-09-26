36deaa8dc66bdf5d3b47adb75e5042189803c4e9
Andrew Chuah <warlordpluto@gmail.com>
2024-09-25 21:15:02 -0400

    Quartz sync: Sep 25, 2024, 9:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The World of Luciradis",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
